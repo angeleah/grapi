14685cc53fdfb3975e5ba1b972b82273a1858a7d
Angeleah Daidone <angeleahdaidone@Angeleahs-Mac-Pro.local>
2014-02-01 20:06:11 -0600

    Refactor and add ability to to process a file.

--- script/import_data.rb
+++ script/import_data.rb
@@ -2,8 +2,34 @@ require "csv"
 
 class DataImporter
 
-  def read_directory(directory)
-    Dir.entries(directory).reject { |file| file == "." || file == ".."}
+  def initialize(directory)
+    @directory = directory
   end
 
+  def read_directory
+    Dir.foreach(@directory).reject { |file| file =~ /^\./}
+  end
+
+  def process_file(file)
+    separator = get_separator(file)
+    CSV.foreach( file, {:col_sep => separator} ) do |row|
+      puts "lastname = #{ row[0]}"
+      puts "firstname = #{ row[1]}"
+      puts "gender = #{ row[2]}"
+      puts "fav_color = #{ row[3]}"
+      puts "birthdate = #{ row[4]}"
+      return "cool"
+    end
+  end
+
+  def get_separator(file)
+    case File.extname(file)
+    when ".csv"
+      ","
+    when ".psv"
+      "|"
+    when ".ssv"
+      " "
+    end
+  end
 end
--- spec/import_data_spec.rb
+++ spec/import_data_spec.rb
@@ -3,10 +3,42 @@ require "script/import_data"
 
 describe 'DataImporter' do
 
-  let(:data_importer) { DataImporter.new }
+  let(:data_importer) { DataImporter.new(File.join("script", "import_files")) }
+  let(:csv_file) { "script/import_files/test.csv" }
+  let(:psv_file) { "script/import_files/test.psv" }
+  let(:ssv_file) { "script/import_files/test.ssv" }
 
   it "reads a directory" do
-    directory = "script/import_files"
-    data_importer.read_directory(directory).should match_array(["test.csv", "test.psv", "test.ssv"])
+    data_importer.read_directory.should match_array(["test.csv", "test.psv", "test.ssv"])
+  end
+
+  context "CSV" do
+    it "gets the file separator" do
+      data_importer.get_separator(csv_file).should == ","
+    end
+
+    it "processes the file" do
+      data_importer.process_file(csv_file).should == "cool"
+    end
+  end
+
+  context "PSV" do
+    it "gets the file separator" do
+      data_importer.get_separator(psv_file).should == "|"
+    end
+
+    it "processes the file" do
+      data_importer.process_file(psv_file).should == "cool"
+    end
+  end
+
+  context "SSV" do
+    it "gets the file separator" do
+      data_importer.get_separator(ssv_file).should == " "
+    end
+
+    it "processes the file" do
+      data_importer.process_file(ssv_file).should == "cool"
+    end
   end
 end
